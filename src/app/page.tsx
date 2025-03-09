6cae506f84f6ce42c4e1bd9107bf11258219e35f
Sajid Malik <sajid_malick@yahoo.com>
2025-03-09 15:49:17 +0000

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
 export default function Home() {
   return (
+    <>
     <h1>Hello! Nextjs in AWS Elastic beanstalk</h1>
+    <ul>
+      <li>The power of Now</li>
+      <li>Deep work</li>
+      <li>The one thing</li>
+      <li>Extreme ownership</li>
+      <li>The magic of thinking big</li>
+      <li>This is marketing</li>
+      <li>Masters of scale</li>
+      <li>The innovator's dilemma</li>
+      <li>Trillion dollar coach</li>
+      <li>Built to last</li>
+      <li>Zero to one</li>
+    </ul>
+    </>
   );
 }
